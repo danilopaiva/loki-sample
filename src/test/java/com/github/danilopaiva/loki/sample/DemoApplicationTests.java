8509326442c7e397faaea9e8b4454fc1506510f3
Danilo Paiva <danilopaivasilva@gmail.com>
2019-09-19 17:30:18 -0300

    fix test and logback

--- src/test/java/com/github/danilopaiva/loki/sample/DemoApplicationTests.java
+++ src/test/java/com/github/danilopaiva/loki/sample/DemoApplicationTests.java
@@ -4,28 +4,25 @@ package com.github.danilopaiva.loki.sample;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
 public class DemoApplicationTests {
 
-	Logger logger = LogManager.getLogger("jsonLogger");
+    Logger logger = LogManager.getLogger("jsonLogger");
 
-	@Test
-	public void contextLoads() {
-		logger.info("info message");
-		logger.debug("debug message");
+    @Test
+    public void contextLoads() {
+        logger.info("info message");
+        logger.debug("debug message");
 
-		try {
-			Integer x = 2/0;
-		} catch (ArithmeticException e) {
-			logger.error("Error: {}", e);
-		}
+        try {
+            Integer x = 2 / 0;
+        } catch (ArithmeticException e) {
+            logger.error("Error: {}", e, e);
+        }
 
-	}
+    }
 
 }
